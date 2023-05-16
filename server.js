37bf77b5af8ded441df95235f713cd4d280af6d0
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-05-16 17:04:22 -0400

    main 4 requests

--- server.js
+++ server.js
@@ -10,6 +10,38 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// POST request handler
+app.post("/", (req, res) => {
+  const { body } = req;
+  // Process the POST request body
+  // ...
+
+  // Example: Log the received data
+  console.log("Received POST data:", body);
+
+  res.send("POST request received");
+});
+
+// PUT request handler
+app.put("/", (req, res) => {
+  const { body } = req;
+  // Process the PUT request body
+  // ...
+
+  // Example: Log the received data
+  console.log("Received PUT data:", body);
+
+  res.send("PUT request received");
+});
+
+// DELETE request handler
+app.delete("/", (req, res) => {
+  // Process the DELETE request
+  // ...
+
+  res.send("DELETE request received");
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
